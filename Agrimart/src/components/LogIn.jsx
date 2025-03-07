f8efebddcf1e58bea19247e4339691268e72fe28
Vaishali48 <vsakariya62@gmail.com>
2025-03-07 17:24:27 +0530

    login page added

---
+++ Agrimart/src/components/LogIn.jsx
@@ -0,0 +1,20 @@
+
+
+const LogIn = () => {
+
+    
+
+    return (
+        <div className='flex justify-center items-center '>
+            <input type="text" placeholder="enter your name"/>
+            <br></br>
+            <br></br>
+            <input type="password" placeholder="enter your password"/>
+            <br></br>
+            <br></br>
+            
+        </div>
+    )
+}
+
+export default LogIn
