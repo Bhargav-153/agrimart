a0c9e60ccb9283048a8771b1f2a4a1d23cc3d3b1
Bhargav sirja <bhargav.sirja162@gmail.com>
2025-09-07 23:09:16 +0530

    mobile view

--- Agrimart/src/components/SubNav.jsx
+++ Agrimart/src/components/SubNav.jsx
@@ -1,12 +1,26 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {
-  FaStore, FaChevronDown, FaSeedling, FaShieldAlt, FaLeaf,
-  FaTractor, FaAppleAlt, FaCloudSun, FaLandmark, FaPlusCircle,
-  FaUserCircle, FaBars, FaTimes
+  FaStore,
+  FaChevronDown,
+  FaSeedling,
+  FaShieldAlt,
+  FaLeaf,
+  FaTractor,
+  FaAppleAlt,
+  FaCloudSun,
+  FaLandmark,
+  FaPlusCircle,
+  FaUserCircle,
+  FaBars,
+  FaTimes,
 } from "react-icons/fa";
 import styles from "./SubNav.module.css";
-import { RouteProfile, RouteRegistration, RouteSchemes } from "@/helpers/RouteName";
+import {
+  RouteProfile,
+  RouteRegistration,
+  RouteSchemes,
+} from "@/helpers/RouteName";
 import { RouteWeather } from "@/helpers/RouteName";
 import { RouteSeeds } from "@/helpers/RouteName";
 import { RouteNursery } from "@/helpers/RouteName";
@@ -29,19 +43,35 @@ const SubNav = () => {
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
+  const closeMenu = () => {
+    setIsMobileNavOpen(false);
+    setIsDropdownOpen(false); // also closes dropdown if open
+  };
 
   return (
     <nav className={styles.subNav}>
       <div className={styles.navContainer}>
         {/* Mobile Menu Toggle Button (Hidden on larger screens) */}
-        <button className={styles.mobileMenuBtn} onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}>
+        <button
+          className={styles.mobileMenuBtn}
+          onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
+        >
           {isMobileNavOpen ? <FaTimes /> : <FaBars />}
         </button>
 
         {/* Navigation Menu */}
-        <ul className={`${styles.navItems} ${isMobileNavOpen ? styles.showMobile : ""}`}>
+        <ul
+          className={`${styles.navItems} ${
+            isMobileNavOpen ? styles.showMobile : ""
+          }`}
+        >
           {/* Shop Dropdown */}
-          <li className={`${styles.shopDropdown} ${isDropdownOpen ? styles.active : ""}`} ref={dropdownRef}>
+          <li
+            className={`${styles.shopDropdown} ${
+              isDropdownOpen ? styles.active : ""
+            }`}
+            ref={dropdownRef}
+          >
             <button
               className={styles.navLink}
               onClick={(e) => {
@@ -54,48 +84,96 @@ const SubNav = () => {
               <FaChevronDown className={isDropdownOpen ? styles.rotate : ""} />
             </button>
 
-            <ul className={`${styles.dropdownMenu} ${isDropdownOpen ? styles.show : ""}`}>
-              <li><Link to={RouteSeeds}><FaSeedling /> Seeds</Link></li>
-              <li><Link to={RouteCropProtection}><FaShieldAlt /> Crop Protection</Link></li>
-              <li><Link to={RouteCropNutrition}><FaLeaf /> Crop Nutrition</Link></li>
-              <li><Link to="/shop/equipment"><FaTractor /> Equipment</Link></li>
-              <li><Link to="/shop/organic"><FaAppleAlt /> Organic</Link></li>
-            </ul> 
+            <ul
+              className={`${styles.dropdownMenu} ${
+                isDropdownOpen ? styles.show : ""
+              }`}
+            >
+              <li>
+                <Link to={RouteSeeds}>
+                  <FaSeedling /> Seeds
+                </Link>
+              </li>
+              <li>
+                <Link to={RouteCropProtection}>
+                  <FaShieldAlt /> Crop Protection
+                </Link>
+              </li>
+              <li>
+                <Link to={RouteCropNutrition}>
+                  <FaLeaf /> Crop Nutrition
+                </Link>
+              </li>
+              <li>
+                <Link to="/shop/equipment">
+                  <FaTractor /> Equipment
+                </Link>
+              </li>
+              <li>
+                <Link to="/shop/organic">
+                  <FaAppleAlt /> Organic
+                </Link>
+              </li>
+            </ul>
           </li>
 
           {/* Other Nav Links */}
           <li>
-            <Link to={RouteWeather} className={styles.navLink}>
+            <Link
+              to={RouteWeather}
+              className={styles.navLink}
+              onClick={closeMenu}
+            >
               <FaCloudSun />
               Weather
             </Link>
           </li>
           <li>
-            <Link to={RouteNursery}className={styles.navLink}>
+            <Link
+              to={RouteNursery}
+              className={styles.navLink}
+              onClick={closeMenu}
+            >
               <FaSeedling />
               Nursery
             </Link>
           </li>
           <li>
-            <Link to={RouteSchemes} className={styles.navLink}>
+            <Link
+              to={RouteSchemes}
+              className={styles.navLink}
+              onClick={closeMenu}
+            >
               <FaLandmark />
               Gov. Schemes
             </Link>
           </li>
           <li>
-            <Link to={RouteRegistration} className={styles.navLink}>
+            <Link
+              to={RouteRegistration}
+              className={styles.navLink}
+              onClick={closeMenu}
+            >
               <FaPlusCircle />
               Add Product
             </Link>
           </li>
           <li>
-            <Link to={RouteProduct} className={styles.navLink}>
+            <Link
+              to={RouteProduct}
+              className={styles.navLink}
+              onClick={closeMenu}
+            >
               <FaPlusCircle />
               Farm Product
             </Link>
           </li>
           <li>
-            <Link to={RouteProfile} className={styles.navLink}>
+            <Link
+              to={RouteProfile}
+              className={styles.navLink}
+              onClick={closeMenu}
+            >
               <FaUserCircle />
               Profile
             </Link>
